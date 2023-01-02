940b42f2753c06a4dfd55a642fc8fc480be0d38d
Boris Shminke <boris@shminke.ml>
2023-01-02 11:40:36 +0100

    Bump to 0.0.12

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,7 +30,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 project = "recommender-systems-course"
 copyright = "2022-2023, Boris Shminke"
-version = "0.0.11"
+version = "0.0.12"
 author = "Boris Shminke"
 
 # -- General configuration ---------------------------------------------------
